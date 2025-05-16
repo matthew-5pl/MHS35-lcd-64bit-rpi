2173af3cd133497f39bba680b522abd2c7ae468f
Matteo Forlani <43993777+matthew-5pl@users.noreply.github.com>
2025-05-16 18:39:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,24 +7,24 @@ cp -rf ./99-calibration.conf  /etc/X11/xorg.conf.d/99-calibration.conf
 cp -rf ./99-fbdev.conf /etc/X11/xorg.conf.d/99-fbdev.conf
 
 # load module on boot
-cp ./mhs35-overlay.dtb /boot/overlays/
-cp ./mhs35-overlay.dtb /boot/overlays/mhs35.dtbo
-echo "hdmi_force_hotplug=1" >> /boot/config.txt
-echo "dtparam=i2c_arm=on" >> /boot/config.txt
-echo "dtparam=spi=on" >> /boot/config.txt
-echo "enable_uart=1" >> /boot/config.txt
-echo "dtoverlay=mhs35:rotate=90" >> /boot/config.txt
-echo "max_framebuffers=2" >> /boot/config.txt
-echo "arm_64bit=1" >> /boot/config.txt
-echo "hdmi_force_hotplug=1" >> /boot/config.txt
-echo "kernel=kernel8l-alt.img" >> /boot/config.txt
-echo "hdmi_group=1" >> /boot/config.txt
-echo "hdmi_mode=4" >> /boot/config.txt
-echo "hdmi_cvt 480 320 60 6 0 0 0" >> /boot/config.txt
-echo "hdmi_drive=2" >> /boot/config.txt
-cp ./cmdline.txt /boot/
+cp ./mhs35-overlay.dtb /boot/firmware/overlays/
+cp ./mhs35-overlay.dtb /boot/firmware/overlays/mhs35.dtbo
+echo "hdmi_force_hotplug=1" >> /boot/firmware/config.txt
+echo "dtparam=i2c_arm=on" >> /boot/firmware/config.txt
+echo "dtparam=spi=on" >> /boot/firmware/config.txt
+echo "enable_uart=1" >> /boot/firmware/config.txt
+echo "dtoverlay=mhs35:rotate=90" >> /boot/firmware/config.txt
+echo "max_framebuffers=2" >> /boot/firmware/config.txt
+echo "arm_64bit=1" >> /boot/firmware/config.txt
+echo "hdmi_force_hotplug=1" >> /boot/firmware/config.txt
+echo "kernel=kernel8l-alt.img" >> /boot/firmware/config.txt
+echo "hdmi_group=1" >> /boot/firmware/config.txt
+echo "hdmi_mode=4" >> /boot/firmware/config.txt
+echo "hdmi_cvt 480 320 60 6 0 0 0" >> /boot/firmware/config.txt
+echo "hdmi_drive=2" >> /boot/firmware/config.txt
+cp ./cmdline.txt /boot/firmware
 
-# Note: Make sure this isn't in yout config.txt. Comment it out if it is:
+# Note: Make sure this isn't in your config.txt. Comment it out if it is:
 #dtoverlay=vc4-fkms-v3d
 
 # touch screen calibration
